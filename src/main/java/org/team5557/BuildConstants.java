8099162893a8774b5267906c0cb3af5052475982
Danny Hagenlocker <dannyhagenlocker@gmail.com>
2023-01-05 18:31:30 -0500

    template commit

---
+++ src/main/java/org/team5557/BuildConstants.java
@@ -0,0 +1,19 @@
+package org.team5557;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKit_CommandBasedProject 2";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2023-01-05 18:19:29 EST";
+  public static final long BUILD_UNIX_TIME = 1672960769272L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants(){}
+}
