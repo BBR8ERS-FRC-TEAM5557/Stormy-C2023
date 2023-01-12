e7b4577fc9cf54b79e1c946d101792d54d72eb89
GreggChase <greggcolbychase@gmail.com>
2023-01-12 07:20:53 -0500

    worked on servoSubsystem

--- src/main/java/org/team5557/BuildConstants.java
+++ src/main/java/org/team5557/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "c2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "ca3dafd3375f9ddb77925046b3c6ea82024731f8";
-  public static final String GIT_DATE = "2023-01-05 18:48:11 EST";
-  public static final String GIT_BRANCH = "swerve-testing";
-  public static final String BUILD_DATE = "2023-01-08 23:00:02 EST";
-  public static final long BUILD_UNIX_TIME = 1673236802384L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "29535b851478ca66191438e897d86a9717829867";
+  public static final String GIT_DATE = "2023-01-11 18:36:27 EST";
+  public static final String GIT_BRANCH = "servo-testing";
+  public static final String BUILD_DATE = "2023-01-12 07:17:56 EST";
+  public static final long BUILD_UNIX_TIME = 1673525876765L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
