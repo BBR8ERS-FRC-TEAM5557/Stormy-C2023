feee6361138e280f98ff20cc399a2e7a15d09085
Danny Hagenlocker <dannyhagenlocker@gmail.com>
2023-01-15 08:57:37 -0500

    comment out teleop heading maintain

--- src/main/java/org/team5557/BuildConstants.java
+++ src/main/java/org/team5557/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "c2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "ca3dafd3375f9ddb77925046b3c6ea82024731f8";
-  public static final String GIT_DATE = "2023-01-05 18:48:11 EST";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "b673d032f3e3d237505065f3bf410e172d26f85d";
+  public static final String GIT_DATE = "2023-01-09 00:35:41 EST";
   public static final String GIT_BRANCH = "swerve-testing";
-  public static final String BUILD_DATE = "2023-01-08 23:00:02 EST";
-  public static final long BUILD_UNIX_TIME = 1673236802384L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-01-09 00:37:16 EST";
+  public static final long BUILD_UNIX_TIME = 1673242636992L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
