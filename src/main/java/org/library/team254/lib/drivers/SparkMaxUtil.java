f017e8e263a162a65fedd39ce780bece4ad67d6c
GreggChase <greggcolbychase@gmail.com>
2023-02-13 07:19:25 -0500

    Update SparkMaxUtil.java

--- src/main/java/org/library/team254/lib/drivers/SparkMaxUtil.java
+++ src/main/java/org/library/team254/lib/drivers/SparkMaxUtil.java
@@ -1,13 +1,13 @@
-package com.team254.lib.drivers;
+package org.library.team254.lib.drivers;
 
-import com.revrobotics.CANError;
+import com.revrobotics.REVLibError;
 
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class SparkMaxUtil {
     // Checks the specified error code for issues.
-    public static void checkError(CANError errorCode, String message) {
-        if (errorCode != CANError.kOK) {
+    public static void checkError(REVLibError errorCode, String message) {
+        if (errorCode != REVLibError.kOk) {
             DriverStation.reportError(message + errorCode, false);
         }
     }
